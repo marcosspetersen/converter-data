40a5def5e0be0120aee98c69c9a87b06650d39c3
marcosspetersen <marcos_petersen@hotmail.com>
2022-08-13 03:17:29 -0300

    Correção do algoritmo

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		
-		String string = "01/05/2018";
+		String string = "01 05 2018";
 		System.out.println(convertDate(string));	
 		
 	}
@@ -17,21 +17,38 @@ public class Program {
 	public static String convertDate(String s) {
 		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy MM");
 		
-		SimpleDateFormat[] format = {
-				new SimpleDateFormat("dd/MM/yyyy"),
-				new SimpleDateFormat("ddMMyyyy")
-		};
+		SimpleDateFormat format10 = new SimpleDateFormat("dd/MM/yyyy");
 		
-		String str = s.replaceAll("\\s+", "");
-		System.out.println(str);
+		SimpleDateFormat format8 = new SimpleDateFormat("ddMMyyyy");
+		
+		//Substituir datas com espaco por /
+		String str = s.replaceAll("\\s+", "/");
+		
 		Date data = null;
-		for (SimpleDateFormat sdf : format) {
-				try {
-					data = sdf.parse(str);
-				} catch (ParseException e) {
-					System.out.println("Tentando outro formato");
-				}
+		
+		//verificando o tamanho do string para aplicar a marcara correta
+		if (str.length() == 10) {
+			
+			//inverter a data caso venha com ano primeiro
+			String[] stringData = str.split("/");
+			if (stringData[0].length() == 4) {
+				str = stringData[2] + "/" + stringData[1] + "/" + stringData[0];
+			}
+			
+			try {
+				data = format10.parse(str);
+			} catch (ParseException e) {
+				System.out.println("Tentando outro formato");
+			}
+			
+		} else {
+			try {
+				data = format8.parse(str);
+			} catch (ParseException e) {
+				System.out.println("Tentando outro formato");
+			}
 		}
+		
 		
 		String dataCorreta;
 		if (data != null) {
